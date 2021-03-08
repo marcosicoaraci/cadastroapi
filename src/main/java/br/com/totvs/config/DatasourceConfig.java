98974f3e91101b8bb7b4a1c33d1fee38b5b68b57
m.chagas <marcos.chagas@totvs.com.br>
2021-03-08 10:16:44 -0300

    Ajustes para receber o banco H2

---
+++ src/main/java/br/com/totvs/config/DatasourceConfig.java
@@ -0,0 +1,23 @@
+package br.com.totvs.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+//@Configuration
+public class DatasourceConfig {
+
+//    @Bean
+//    public DataSource datasource() {
+//        return DataSourceBuilder.create()
+//                .driverClassName("org.h2.Driver")
+//                .url("h2:mem:testdb")
+//                .username("user1")
+//                .password("pass")
+//                .build();
+//    }
+
+}
