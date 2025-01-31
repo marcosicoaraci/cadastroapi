8010c315786d33887cbff57ff32feaf8b1ecdc59
marcos <marcos.roberto.icoaraci@gmail.com>
2025-01-31 12:35:27 -0300

    Ajustes no SwaggerConfig

--- src/main/java/br/com/totvs/config/SwaggerConfig.java
+++ src/main/java/br/com/totvs/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package br.com.totvs.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,41 +11,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("br.com.totvs.rest"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(this.apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("Cadastro API")
-//                .description("Crud Simples de Cadastro de Contatos")
-//                .version("1.0.0")
-//                .license("Apache License Version 2.0")
-//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-//                        .contact(new Contact("Marcos Chagas",
-//                                "https://github.com/marcosicoaraci/cadastroapi",
-//                                "marcos.roberto.icoaraci@gmail.com"))
-//                        .build();
-//    }
-//
-//
+
     @Bean
-    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
 
                 .info(new Info()
                         .title("Cadastro API")
-                        .version(appVersion)
-                        .description(appDesciption)
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-
+                        .description("Crud Simples de Cadastro de Contatos")
+                        .version("1.1.0")
+                        .contact(new Contact()
+                                .name("Marcos Chagas")
+                                .url("https://github.com/marcosicoaraci/cadastroapi")
+                                .email("marcos.roberto.icoaraci@gmail.com"))
+                        .license(new License()
+                                .name("Apache License Version 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0"))
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Perfil no Linkedin")
+                        .url("https://www.linkedin.com/in/marcos-roberto-0741a776/"));
     }
 
 }
